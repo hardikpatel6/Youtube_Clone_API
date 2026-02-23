8dcb0329f7c758cefca85c8b10a7cc91ad73867c
hardikpatel6 <patelhardikp6@gmail.com>
2026-02-23 18:43:26 +0530

    change_cors_policy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,7 @@ const port: number | string = process.env.PORT || 4000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://youtubec-liard.vercel.app/login"], // frontend URL
+    origin: ["http://localhost:5173","https://youtubec-liard.vercel.app"], // frontend URL
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
